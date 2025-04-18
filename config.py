c0509601fdb6354e6efbea077f9a0a9adec20c1d
unknown <komal.hukums@gmail.com>
2025-04-18 17:24:19 +0100

    Configured environment file

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LINKEDIN_URL = os.getenv('LINKEDIN_URL')
--- tests/test_hit_url.py
+++ tests/test_hit_url.py
@@ -1,8 +1,11 @@
 import pytest
 
+import config
+
+
 @pytest.mark.usefixtures("setup")
 class Test_hit_url:
     def test_hit_url(self):
-        self.driver.get('https://google.com')
-        assert 'Google' in self.driver.title
+        self.driver.get(config.LINKEDIN_URL)
+        assert 'LinkedIn' in self.driver.title
 
